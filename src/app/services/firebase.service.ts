d29fe5584b89412885d3bd28c2d05803512478aa
Jorciney Dias Chaveiro <jorcineyadorador@hotmail.com>
2018-07-31 19:12:57 +0200

    Fixed delete and generateId

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,14 +1,17 @@
 import {Injectable} from '@angular/core';
 import {AngularFireDatabase, AngularFireList} from 'angularfire2/database';
-import {map} from 'rxjs/operators';
+import {takeUntil} from 'rxjs/operators';
 import {Todo} from '../model/todo';
 import {Observable} from 'rxjs/internal/Observable';
+import 'rxjs-compat/add/operator/max';
+import {Subject} from 'rxjs/Subject';
 
 @Injectable()
 export class FirebaseService {
   todosKeyValues: Map<string, Todo> = new Map();
   allTodos: Observable<Array<any>>;
   private firebaseList: AngularFireList<any>;
+  stopSubscription: Subject<boolean> = new Subject<boolean>();
 
   constructor(private databaseFB: AngularFireDatabase) {
     this.fetchKeyValues();
@@ -30,12 +33,15 @@ export class FirebaseService {
   }
 
   public deleteTodo(id: number): void {
-    this.databaseFB.list('/').snapshotChanges().subscribe(
-      value => value.forEach(todo => {
-        if (todo.payload.val().id === id) {
-          this.databaseFB.object('/' + todo.payload.key).remove();
-        }
-      })
+    this.databaseFB.list('/').snapshotChanges().pipe(takeUntil(this.stopSubscription)).subscribe(
+      value => {
+        value.forEach(todo => {
+          if (todo.payload.val().id === id) {
+            this.databaseFB.object('/' + todo.payload.key).remove();
+          }
+        });
+        this.stopSubscription.next(true);
+      }
     );
   }
 
@@ -70,9 +76,8 @@ export class FirebaseService {
 
   private generateId() {
     let id = -1;
-    this.getAllTodosFromDB().pipe(
-      map(tdos => id = Math.max(...tdos.map(t => t.id)) + 1)
-    );
+    const values = this.todosKeyValues.values();
+    id = Math.max.apply(Math, Array.from(values).map(todo => todo.id)) + 1;
     return id;
   }
 }
